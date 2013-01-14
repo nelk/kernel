782421cab946fb090a02109d4ef293d7837964bd
Sanjay Menakuru <balasanjay@gmail.com>
2013-01-14 16:32:51 -0500

    added a newline at end of file, because compiler is retard

--- type.h
+++ type.h
@@ -1,3 +1,6 @@
+#ifndef NULL
+#define NULL ((void *)0x0)
+#endif
 
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
@@ -6,7 +9,3 @@ typedef unsigned long uint64_t;
 typedef short int16_t;
 typedef int int32_t;
 typedef long int64_t;
-
-#ifndef NULL
-#define NULL ((void *)0x0)
-#endif
