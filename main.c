ae879de8b0b00f6f5947dc39fc38530980faa7aa
JonathanWei <jon.wei92@gmail.com>
2013-01-14 21:24:49 -0500

    Added additional compilation line in tests, and included memory initialisation in main.

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <LPC17xx.h>
 #include "uart_polling.h"
+#include "mem.h"
 
 int main () {
 	SystemInit();
 	uart0_init();
 	uart0_put_string("Hello World!\n\r");
+	k_memInit();
 	return 0;
 }
