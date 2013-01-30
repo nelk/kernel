d2d7a0e3253b9f2f22260ab53fc6d408792aaeff
Sanjay Menakuru <balasanjay@gmail.com>
2013-01-30 12:54:46 -0500

    created a FIFO ordering for PCBs with the same priority

--- prq.c
+++ prq.c
@@ -8,25 +8,39 @@
  * http://tip.golang.org/src/pkg/container/heap/heap.go
  */
 
-void prqInit(PRQ *q, PCB **prStore, uint32_t prStoreSize) {
+void prqInit(PRQ *q, PRQEntry *prStore, uint32_t prStoreSize) {
   q->heap = prStore;
-  q->size = 0;
+  q->len = 0;
   q->cap = prStoreSize;
 }
 
-PCB* prqTop(PRQ *q) {
-  if (q->size == 0) {
-    return NULL;
-  }
-  return q->heap[0];
+uint32_t prqNextSeq(PRQ *q) {
+  return (q->seq)++;
 }
 
-uint32_t prqLess(PCB *a, PCB *b) {
-  return a->priority < b->priority;
+PCB* prqTop(PRQ *q) {
+  if (q->len == 0) {
+    return NULL;
+  }
+
+  return q->heap[0].pcb;
+}
+
+uint32_t prqLess(PRQEntry a, PRQEntry b) {
+  if (a.pcb->priority != b.pcb->priority) {
+    return a.pcb->priority < b.pcb->priority;
+  }
+
+  if (a.seqNumber != b.seqNumber) {
+    return a.seqNumber < b.seqNumber;
+  }
+
+  // This is a last-resort fallback. This case should never be triggered
+  return a.pcb < b.pcb;
 }
 
 void prqUp(PRQ *q, uint32_t j) {
-  PCB *temp;
+  PRQEntry temp;
   while (1) {
     uint32_t i = (j - 1) / 2; // parent
     if (i == j || !prqLess(q->heap[j], q->heap[i])) {
@@ -43,8 +57,8 @@ void prqDown(PRQ *q, uint32_t i) {
   uint32_t j1 = 0;
   uint32_t j = 0;
   uint32_t j2 = 0;
-  uint32_t n = q->size;
-  PCB *temp;
+  uint32_t n = q->len;
+  PRQEntry temp;
 
   while (1) {
     j1 = 2*i + 1;
@@ -67,22 +81,26 @@ void prqDown(PRQ *q, uint32_t i) {
 }
 
 uint32_t prqAdd(PRQ *q, PCB *pcb) {
-  if (q->size >= q->cap) {
+  PRQEntry newEntry;
+
+  if (q->len >= q->cap) {
     return 1;
   }
-  q->heap[q->size] = pcb;
-  ++(q->size);
-  prqUp(q, q->size - 1);
+
+  newEntry.pcb = pcb;
+  newEntry.seqNumber = prqNextSeq(q);
+
+  q->heap[q->len] = newEntry;
+  ++(q->len);
+  prqUp(q, q->len - 1);
   return 0;
 }
 
 PCB *prqRemove(PRQ *q, uint32_t i) {
-  PCB *removed = q->heap[i];
-  q->heap[i] = q->heap[q->size - 1];
-  --(q->size);
+  PCB *removed = q->heap[i].pcb;
+  q->heap[i] = q->heap[q->len - 1];
+  --(q->len);
   prqDown(q, i);
   prqUp(q, i);
   return removed;
 }
-
-
