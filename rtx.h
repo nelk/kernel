ef1e24aa2dccacdb7cac9a79803358442e837317
shalecraig <shalecraig@gmail.com>
2013-01-30 22:47:56 -0500

    Another one done. One left.

--- rtx.h
+++ rtx.h
@@ -8,18 +8,18 @@
 // releaseProcessor
 extern uint32_t bridge_releaseProcessor(void);
 #define release_processor() _releaseProcessor((uint32_t)bridge_releaseProcessor)
-extern uint32_t _releaseProcessor(uint32_t k_func) __SVC_0
+uint32_t __SVC_0 _releaseProcessor(uint32_t k_func);
 
 
 // acquireMemoryBlock
 extern void *bridge_acquireMemoryBlock(void);
 #define request_memory_block() _acquireMemoryBlock((uint32_t)bridge_acquireMemoryBlock)
-extern uint32_t _acquireMemoryBlock(uint32_t k_func) __SVC_0
+uint32_t __SVC_0 _acquireMemoryBlock(uint32_t k_func);
 
 // releaseMemoryBlock
 extern uint32_t bridge_releaseMemoryBlock(void *blk);
 #define release_memory_block(blk) _releaseMemoryBlock((uint32_t)bridge_releaseMemoryBlock, blk)
-extern uint32_t _releaseMemoryBlock(uint32_t k_func, void *blk) __SVC_0
+uint32_t __SVC_0 _releaseMemoryBlock(uint32_t k_func, void *blk);
 
 
 #endif
