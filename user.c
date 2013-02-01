86162482f1bfbcacd3fedb41b960b0d3c1f05c72
Sanjay Menakuru <balasanjay@gmail.com>
2013-01-31 23:04:38 -0500

    added CPU intensive, fibonacci-calulating, process.
    also implemented a really simple itoa

--- user.c
+++ user.c
@@ -36,4 +36,51 @@ void schizophrenicProcess(void) {
 	}
 }
 
-void memMunchProcess(void) {}
+void itoa(char *buf, uint8_t i) {
+	*(buf++) = (i / 100) + '0';
+	i %= 100;
+	*(buf++) = (i / 10) + '0';
+	i %= 10;
+	*(buf++) = i + '0';
+}
+
+void fibProcess(void) {
+	uint8_t temp;
+	uint8_t cur;
+	uint8_t prev;
+	uint8_t idx;
+	char *str = "fib(000) = 000";
+	uint8_t *buf = (uint8_t *)request_memory_block();
+	uint8_t *iterl = NULL;
+	uint8_t *iterr = NULL;
+
+	iterl = str;
+	iterr = buf;
+	while (*iterl != '\0') {
+		*(iterr++) = *(iterl++);
+	}
+	*iterr = '\0';
+
+	while (1) {
+		prev = 1;
+		cur = 1;
+		idx = 0;
+		while (cur < 200) {
+			temp = prev;
+			prev = cur;
+			cur = cur + temp;
+			idx++;
+
+			itoa(buf + 4, idx);
+			itoa(buf + 11, cur);
+
+			uart1_put_string(buf);
+
+			if (idx % 5 == 0) {
+				release_processor();
+			}
+		}
+
+		release_processor();
+	}
+}
