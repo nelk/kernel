58658aa4c6649816f39c3c84a64da2402a8cca51
shalecraig <shalecraig@gmail.com>
2013-03-19 23:00:17 -0400

    re-ordered vars in one of the declarations

--- mem.c
+++ mem.c
@@ -95,7 +95,7 @@ uint32_t k_getAlignedEndAddress(uint32_t end, uint32_t blockSizeBytes) {
 
 // Only for use during initialization. Extracted for testing purposes.
 // Note that blockSizeBytes must be greater than or equal to
-// sizeof(FreeBlock), or else madness will ensue.
+// sizeof(FreeBlock), or madness will ensue.
 void k_memInfoInit(
         MemInfo *memInfo,
         uint32_t startAddr,
@@ -126,10 +126,10 @@ void k_memInfoInit(
 // passed in owner id (oid).
 uint32_t k_acquireMemoryBlock(MemInfo *memInfo, ProcId oid) {
     FreeBlock *curFirstFree = NULL;
-    uint32_t ret = 0;
-    ProcId *header = NULL;
     uint8_t didAllocateHeader = 0;
+    ProcId *header = NULL;
     uint32_t memOffset = 0;
+    uint32_t ret = 0;
 
     // Try free list, first
     if (memInfo->firstFree != NULL) {
