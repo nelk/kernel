d84c229a1f530a422c5174ab1fc146ebd78ad5a0
shalecraig <shalecraig@gmail.com>
2013-03-25 00:31:24 -0400

    Removed some of the TODOs.

--- bridge.c
+++ bridge.c
@@ -71,7 +71,7 @@ int8_t bridge_releaseMemoryBlock(void *blk) {
 
 int8_t bridge_sendMessage(uint8_t pid, Envelope *envelope) {
     int8_t releaseProcessor = k_sendMessage(&gMemInfo, &gProcInfo, envelope, gProcInfo.currentProcess->pid, pid);
-    if (releaseProcessor == -1) {   // TODO: Replace with enums.
+    if (releaseProcessor == -1) {   // Release processor
         k_releaseProcessor(&gProcInfo, &gMemInfo, &gMessageInfo, &gClockInfo, MESSAGE_SENT);
     }
     return releaseProcessor;
